608eb11f100b8306ecc44e6875137d8044375978
Budiprigunanda <74167295+Budiprigunanda@users.noreply.github.com>
2020-11-30 16:03:04 +0700

    Add files via upload

---
+++ ujianlagi.py
@@ -0,0 +1,35 @@
+# soal ujian lagi nomer 1
+
+# find_short = input('find_short: ')
+# i = find_short
+# l = i.split()
+# # print(min(l, key=len))
+# mines = len(min(l, key=len))
+# print('The shortest word has', mines, 'char(s)')
+
+# many people get up early in the morning
+# Every office getting newest monitor
+# Create new file after this morning
+
+# soal ujian lagi nomer 2
+
+
+# def persistence(n):
+#     times = 0
+#     product = 1
+#     while len(str(n)) != 1:
+#         for i in list(str(n)):
+#             product *= int(i)
+#         n = product
+#         product = 1
+#         times += 1
+#     return times
+
+
+# print(persistence(39))
+# print(persistence(999))
+# print(persistence(4))
+# print(persistence(-12))
+    
+
+
